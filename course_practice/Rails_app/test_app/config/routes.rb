4e3a65ac8d50c301364b1d0ca9e9efbdccfc757f
Marc Medhat <marcmedhat6211@gmail.com>
2021-08-28 12:06:25 +0200

    articles CRUD

--- course_practice/Rails_app/test_app/config/routes.rb
+++ course_practice/Rails_app/test_app/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  #dyh bet generate a whole bunch of routes for the articles
+  resources :articles
   #go to home action in pages controller
   root 'pages#home'
   get 'about', to: 'pages#about'
